de649230c3bb8337bee11915934495ea7be1462b
insanmulyana <149320890+insanmulyana@users.noreply.github.com>
2025-12-30 20:05:24 +0700

    app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ if input_nis and input_tgl:
 
 #paneladmin
 st.write("")
-with st.expander("Panel Admin"):
+with st.expander("Panel Admin (hanya diakses admin sekolah"):
     pw = st.text_input("Password", type="password")
     if pw == "admin123":
         st.write("### Rekapitulasi Verifikasi")
@@ -114,3 +114,4 @@ with st.expander("Panel Admin"):
             csv = st.session_state.df_siswa.to_csv(index=False).encode('utf-8')
 
             st.download_button("Download Data (CSV)", csv, "hasil_verifikasi.csv", "text/csv")
+
